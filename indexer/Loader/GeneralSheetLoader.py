7da574d9b3b69567d21e180f709feee7d299924a
ketabi <ketabi>
2023-04-24 09:55:52 +0330

    index to elastic

--- indexer/Loader/GeneralSheetLoader.py
+++ indexer/Loader/GeneralSheetLoader.py
@@ -4,10 +4,7 @@ import numpy as np
 
 from base_logger import logger
 
-logger.info('HIIIIIIIIIIIIIIII')
-
-
-
+print('HIIIIIIIIIIIIIIII')
 
 MALE_GENDER = 1
 FEMALE_GENDER = 2
@@ -16,7 +13,7 @@ FEMALE_GENDER = 2
 class GeneralSheetLoader(object):
 
     def __init__(self, file_path, sheet_name, data_types=None, skiprows=0):
-        logger.info(f'+======================== creating a loader: {sheet_name}')
+        # print(f'Creating a loader: {sheet_name}')
         self.file_path = file_path
         self.sheet_name = sheet_name
         self.removed_cols = []
@@ -24,15 +21,15 @@ class GeneralSheetLoader(object):
         self.df = self.csv_data
         self._set_unique_ids()
         self.df = self._process_irpc()
-        self.df = self._remove_duplicate()
+        self.df_unique = self._remove_duplicate()
         self.df = self._process_date_fields()
         # self.df[IRPC_COLUMN_NAME] = self.df[IRPC_COLUMN_NAME].fillna(-1).astype(int)
         self.df = self._rename_cols()
 
     def _read_data(self, data_types, skip_rows):
-        # logger.info('='*100)
-        # logger.info()
-        # logger.info("Reading this file  {} ".format(self.file_path))
+        # print('='*100)
+        # print()
+        # print("Reading this file  {} ".format(self.file_path))
         if self.file_path.endswith('.csv'):
             # parse csv with pandas
             csv_data = pd.read_csv(self.file_path,
@@ -53,8 +50,9 @@ class GeneralSheetLoader(object):
         # # chunksize=chunksize
         # )
 
-        logger.info('File {} has this shape:{}'.format(self.file_path, csv_data.shape))
-        logger.info('columns: {}'.format(csv_data.columns))
+        # print('File {} has this shape:{}'.format(self.file_path, csv_data.shape))
+        print('{}'.format(csv_data.shape))
+        # print('columns: {}'.format(csv_data.columns))
 
         self.csv_data = csv_data
 
@@ -71,84 +69,86 @@ class GeneralSheetLoader(object):
         :return:
         """
         # self.ids_unique = self.csv_data[IRPC_COLUMN_NAME].unique()
-        # logger.info('No. unique ids :{}'.format(len(self.ids_unique)))
+        # print('No. unique ids :{}'.format(len(self.ids_unique)))
 
     def describe(self):
         # -----------------------------------------------------------------------
         # DESCRIBE
         # -----------------------------------------------------------------------
-        logger.info('\n' + '-' * 50)
-        logger.info('Describe')
-        logger.info('-' * 50)
+        print('\n' + '-' * 50)
+        print('Describe')
+        print('-' * 50)
 
-        logger.info(self.csv_data.describe())
+        print(self.csv_data.describe())
 
     def info(self):
         # -----------------------------------------------------------------------
         # INFO
         # -----------------------------------------------------------------------
-        logger.info('\n' + '-' * 50)
-        logger.info('info')
-        logger.info('-' * 50)
+        print('\n' + '-' * 50)
+        print('info')
+        print('-' * 50)
 
-        logger.info(self.csv_data.info())
+        print(self.csv_data.info())
 
     def value_count(self):
         # -----------------------------------------------------------------------
         # VALUE COUNT
         # -----------------------------------------------------------------------
-        logger.info('\n' + '-' * 50)
-        logger.info('value count')
-        logger.info('-' * 50)
+        print('\n' + '-' * 50)
+        print('value count')
+        print('-' * 50)
 
-        # logger.info('value counts for {} column'.format(IRPC_COLUMN_NAME))
-        # logger.info(self.csv_data[IRPC_COLUMN_NAME].value_counts(
+        # print('value counts for {} column'.format(IRPC_COLUMN_NAME))
+        # print(self.csv_data[IRPC_COLUMN_NAME].value_counts(
         #     sort=True,
         #     # normalize=True
         # ))
 
     def _process_irpc(self):
         df = self.df
-        logger.info(f'input df shape: {df.shape}')
+        # print(f'input df shape: {df.shape}')
 
         # This returns a pandas Series contain true for IRPC's which is not null
         null_rows = df.loc[~df["IRPC"].notnull()]
-        logger.info(f'There are {len(null_rows)} rows which have null IRPC')
+        # if not null_rows.empty:
+        print(f'{len(null_rows)} rows have null IRPC')
         self.null_irpc_count = len(null_rows)
 
         # Remove null IRPC's
         df = df.loc[df["IRPC"].notnull()]
 
         # convert type of IRPC to an integer instead of float
-        logger.info(f"IRPC column has {df['IRPC'].dtypes} type")
+        # print(f"IRPC column has {df['IRPC'].dtypes} type")
         df["IRPC"] = df["IRPC"].astype(np.int64)
 
-        logger.info(f'input df shape: {df.shape}')
+        # print(f'input df shape: {df.shape}')
         return df
 
     def _rename_cols(self):
         prefix = self.sheet_name + '.'
-        columns = {d: prefix + d for d in list(self.df.columns) if d != 'IRPC'}
+        columns = {d: prefix + d.split('.')[0] for d in list(self.df.columns) if d != 'IRPC'}
         self.renamed_cols = True
-        logger.info(f'renamed and prefix added: {self.sheet_name}')
+        # print(f'renamed and prefix added')
         return self.df.rename(columns=columns)
 
     def _process_date_fields(self):
         df = self.df
+        removed_cols = []
         if 'InterviewDate' in self.df:
             df.drop('InterviewDate', axis=1, inplace=True)
             self.removed_cols.append('InterviewDate')
-            logger.info('InterviewDate removed')
         if 'BirthDate' in self.df:
             df.drop('BirthDate', axis=1, inplace=True)
             self.removed_cols.append('BirthDate')
-            logger.info('InterviewDate removed')
-            logger.info('BirthDate removed')
+        print('removed cols:' + str(self.removed_cols))
         return df
 
     def _remove_duplicate(self):
-        logger.info('duplicated removed: --------')
         duplicated = self.df[self.df.duplicated(['IRPC'], keep=False)]
-        logger.info(duplicated)
-        self.duplicated_count = len(duplicated)
+
+        duplicated_count = len(duplicated)
+        print(f'{duplicated_count} duplicated')
+        self.duplicated_count = duplicated_count
         return self.df[~self.df.duplicated(['IRPC'], keep='first')]
+
--- indexer/main_indexer_new.py
+++ indexer/main_indexer_new.py
@@ -2,11 +2,47 @@ from Loader.GeneralSheetLoader import GeneralSheetLoader
 from elasticsearch import Elasticsearch
 import urllib3
 from base_logger import logger
+from functools import reduce # only in Python 3
+import os
+import pandas as pd
+
+es_user = os.environ.get('VST_ELASTIC_USER', 'elastic')
+es_pass = os.environ.get('VST_ELASTIC_PASS', 'elastic_pass')
+data_path = os.environ.get('VST_DATA_PATH', '../../data-fasa/summer-1401/')
 
 
 logger.warning('This is a Warning')
 
+
 files = [
+        ("AmputationHistory.xlsx", 1),
+        ("AnthropometericExam.xlsx", 1),
+        ("BloodPressure.xlsx", 1),
+("ChronicDisease1.xlsx", 1),
+("ChronicDisease2.xlsx", 1),
+("EmploymentHistory.xlsx", 1),
+("EmploymentStatus.xlsx", 1),
+("FamilyHistory.xlsx", 1),
+("Final_MET_Score.xlsx", 1),
+("GeneralData.xlsx", 1),
+("GFR_Score.xlsx", 1),
+# ("GPD.xlsx", 1),
+("Gynecologist.xlsx", 1),
+("Habit.xlsx", 1),
+("HFHistory.xlsx", 1),
+("ImportantMedicationAndDiseases.xlsx", 1),
+("LifeStyle.xlsx", 1),
+("NBS.xlsx", 1),
+("OralHealth.xlsx", 1),
+("PhysicalActivity.xlsx", 1),
+("PhysicalExam.xlsx", 1),
+("SleepAssessment.xlsx", 1),
+("Socioeconomic.xlsx", 1),
+("UsedDrug.xlsx", 1),
+("Wealth_Score.xlsx", 1)
+    ]
+
+
     # (9536,  'labTestresult', 1),
     # (10145, 'AnthropometericExam', 1),
     # (10145, 'BioBank', 1),
@@ -17,15 +53,15 @@ files = [
     # (10145, 'Final_MET_Score', 1),
     # (10145, 'Gynecologist', 1),
     # (10145, 'Habit', 1),
-    (10145, 'LifeStyle', 1),
-    # (10145, 'MobileUse', 1),
-    # (10145, 'OralHealth', 1),
-    # (10145, 'Pesticide', 1),
-    # (10145, 'PhysicalActivity', 1),
-    # (10145, 'PhysicalExam', 1),
-    # (10145, 'SleepAssessment', 1),
-    # (10145, 'Socioeconomic', 1),
-    # (10145, 'WaterUsed', 1),
+    # (10145, 'LifeStyle', 1),
+    #(10145, 'MobileUse', 1),
+    #(10145, 'OralHealth', 1),
+    #(10145, 'Pesticide', 1),
+    #(10145, 'PhysicalActivity', 1),
+    #(10145, 'PhysicalExam', 1),
+    #(10145, 'SleepAssessment', 1),
+    #(10145, 'Socioeconomic', 1),
+    #(10145, 'WaterUsed', 1),
     #
     # (10160, 'GFR_Score', 0),
     # (16612, 'UsedDrug', 0),
@@ -34,14 +70,16 @@ files = [
     # (265656, 'FamilyHistory', 0),
     # (140185, 'FoodHabit', 0),
     # (110935, 'SupplementUsed', 0),
-]
+#]
 
 sheets = {}
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-es = Elasticsearch(["https://elastic:elastic-password@localhost:9200/"], verify_certs=False)
+elastic_url = f'https://{es_user}:{es_pass}@localhost:9200/'
+print(f'elastic_url: {elastic_url}')
+es = Elasticsearch([elastic_url], verify_certs=False, request_timeout=30)
 
 
 def index_in_elastic(df, index_name):
@@ -56,24 +94,41 @@ def index_in_elastic(df, index_name):
             print(e, idx)
     print(f'{ignored} rows failed to be indexed')
 
-DATA_FOLDER_PREFIX = "./data/summer-1401/"
 # DATA_FOLDER_PREFIX = "./data/csv/"
 
 
 def sheet_loader(f):
-    rows_count, sheet_name, skipped_rows = f
-    df_loader = None
-    if rows_count <= 10145:
-        df_loader = GeneralSheetLoader(f"{DATA_FOLDER_PREFIX}{sheet_name}.xlsx", sheet_name, skiprows=skipped_rows)
-    return sheet_name, df_loader
+    sheet_name, skipped_rows = f
+    print(f'{"-"*10}{sheet_name}')
+    df_loader = GeneralSheetLoader(f'{data_path}{sheet_name}', sheet_name, skiprows=skipped_rows)
+    return sheet_name.split('.')[0], df_loader
+    
 
+data_frames = []
+dfs_unique = []
+unique_dfs_name = []
+cols = []
 
 for idx, f in enumerate(files):
-    sheet_name, df_loader = sheet_loader(f)
-    if df_loader is not None:
-        # index_in_elastic(df_loader.df, f'new_cohort_{sheet_name.lower()}')
-        sheets[sheet_name] = df_loader
+    sheet_name, loader = sheet_loader(f)
+    if loader.duplicated_count == 0:
+        unique_dfs_name.append(sheet_name)
+        dfs_unique.append(loader.df)
+
+    cols.extend(loader.df.columns)
+    print(f'{sheet_name}, {loader.df.shape}')
+    if loader is not None:
+        index_in_elastic(loader.df, f'new_cohort_2_{sheet_name.lower()}')
+        # sheets[sheet_name] = df_loader
+        data_frames.append(loader.df)
         #if idx > 0:
         #    df.merge
 
-# index_in_elastic(df_loader.df, f'new_cohort_{sheet_name.lower()}')
+print(f'{len(unique_dfs_name)} dfs has unique IRPCs')
+print(f'{len(data_frames)} total dfs')
+print(f'{len(cols)} cols, {len(set(cols))} uniq cols')
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['IRPC'],
+                                            how='outer'), dfs_unique)
+index_in_elastic(df_merged, 'new_cohort_2_all')
+logger.warning(df_merged.shape)
+# print(data_frames)
